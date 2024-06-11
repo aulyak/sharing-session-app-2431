fe4329f64bfbfcb73462406b6c7fc40b66b1ba36
DESKTOP-PPEVJ58\Kuya 2 <aulya.khatulistivani@gmail.com>
2024-06-12 02:05:46 +0700

    first commit

---
+++ dest/desktop/js/desktop.js
@@ -0,0 +1,33 @@
+(() => {
+  // indent will be adjusted, force semi
+  'use strict';
+
+  kintone.events.on('app.record.index.show', (e) => {
+    // var will be changed to const
+    const message = 'this is a message';
+
+    // not automatically fixed
+    const unusedVar = null;
+
+    // let will be changed to const
+    const nestedObject = {
+      nama: 'aulya',
+      pets: [
+        { type: 'cat', name: 'leah' },
+        { type: 'cat', name: 'sean' },
+        { type: 'dog', name: 'dogi' },
+        { type: 'bird', name: 'birdi' },
+        { type: 'snake', name: 'sneki' },
+        { type: 'lion', name: 'simba' },
+      ],
+    };
+
+    // var will be changed to const
+    const anotherMessage =
+      'this is a very very very very very very very very very very very very very very very very very very very very very very very very very very very very long message';
+
+    console.log(nestedObject);
+    alert(message);
+    alert(anotherMessage);
+  });
+})();
